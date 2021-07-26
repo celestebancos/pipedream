ca348fec6051c7c0e00f5367f4b643a80962f7fb
Celeste Bancos <celestebancos@gmail.com>
2021-07-26 08:10:10 -0400

    Copy Sync Account file to use as a starting point for Sync Record

---
+++ components/airtable/actions/sync-record/sync-record.js
@@ -0,0 +1,62 @@
+const airtable = require("../../airtable.app.js");
+
+const DEAL_TRACKER_BASE_ID = 'appquN7zlOyyK8sMF'
+
+module.exports = {
+	name: 'Sync Record',
+	description: 'Update or create a record as needed to sync with an external source.',
+	key: 'sync_record',
+	version: '0.0.1',
+	type: 'action',
+	props: {
+		airtable,
+		source_record : {
+			type: 'object',
+			label: 'Source Record',
+		}
+	},
+	methods: {
+		table(){
+			return this.airtable.base(DEAL_TRACKER_BASE_ID)('Accounts')
+		},
+		async checkForExistingRecord(entity_type, entity_id){
+			const config = {
+				filterByFormula: `{QB ${entity_type} ID} = ${entity_id}`,
+			}
+			const data = []
+			await this.table().select(config).eachPage((records, fetchNextPage) => {
+				records.forEach(record => {
+					data.push(record._rawJson)
+				})
+				fetchNextPage()
+			})
+			const first_matching_record = data[0]
+			return first_matching_record
+		},
+		async createNewRecord(entity_type, record_data){
+			const qb_name_field_name = `QB ${entity_type} Name`  // QB Customer Name or QB Vendor Name
+			const qb_id_field_name = `QB ${entity_type} ID`			 // QB Customer ID or QB Vendor ID
+			const fields = {
+				'Name': record_data.DisplayName,
+				[qb_name_field_name]: record_data.DisplayName,
+				[qb_id_field_name]: record_data.Id,
+			}
+			const airtable_record = await this.table().create(fields)
+			return airtable_record
+		}
+	},
+	async run(){
+		const {entity_type, record_data} = this.source_record
+		const entity_id = record_data.Id
+
+		const existing_record = await this.checkForExistingRecord(entity_type, entity_id)
+		if(existing_record){
+			//update existing record?
+			return existing_record
+		} else {
+			//create new record
+			const new_record = await this.createNewRecord(entity_type, record_data)
+			return new_record
+		}
+	},
+}
