4be93b2d15b91708ba0b6c5faae193853bf5148a
Celeste Bancos <celestebancos@gmail.com>
2021-08-16 22:06:02 -0400

    Add props and uploadFile() method

--- components/zoho_docs/actions/upload-file/upload-file.js
+++ components/zoho_docs/actions/upload-file/upload-file.js
@@ -1,14 +1,55 @@
 const zoho_docs = require('../../zoho_docs.app.js')
+const axios = require('axios')
+const FormData = require('form-data')
+const fs = require('fs')
 
 module.exports = {
 	name: 'Upload File',
 	description: 'Upload a file to the specified folder.',
 	key: 'upload_file',
-	version: '0.0.0',
+	version: '0.0.3',
 	type: 'action',
 	props: {
 		zoho_docs,
+		folder_id: {
+			type: 'string',
+			label: 'Folder ID',
+		},
+		file_path: {
+			type: 'string',
+			label: 'File Path',
+		},
+		file_name: {
+			type: 'string',
+			label: 'File Name',
+		},
 	},
-	methods: {},
-	async run(){},
+	methods: {
+		async uploadFile(folder_id, file_path, file_name){
+    const data = new FormData()
+    data.append('content', await fs.createReadStream(file_path))
+    const response = await axios({
+        method: "post",
+        url: `https://apidocs.zoho.com/files/v1/upload`,
+        headers: {
+            "Authorization": `Zoho-oauthtoken ${this.zoho_docs.$auth.oauth_access_token}`,
+            'Content-Type': 'multipart/form-data',
+            ...data.getHeaders(),
+        },
+        params: {
+            filename: file_name,
+            fid: folder_id,
+        },
+        data: data,
+    })
+    const [{documentname}, {uploaddocid}] = response.data.response[2].result
+    return {
+        name: documentname,
+        id: uploaddocid,
+    }
+}
+	},
+	async run(){
+		return await this.uploadFile(this.folder_id, this.file_path, this.file_name)
+	},
 }
