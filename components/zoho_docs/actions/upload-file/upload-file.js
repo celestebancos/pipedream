57fb773264ded08df47b2b855abb9d07e9658d82
Celeste Bancos <celestebancos@gmail.com>
2021-08-26 22:27:09 -0400

    Get subfolders of first top-level folder dynamically

--- components/zoho_docs/actions/upload-file/upload-file.js
+++ components/zoho_docs/actions/upload-file/upload-file.js
@@ -7,7 +7,7 @@ module.exports = {
   name: "Upload File",
   description: "Upload a file to the specified folder.",
   key: "upload_file",
-  version: "0.2.16",
+  version: "0.2.41",
   type: "action",
   props: {
     zohoDocs,
@@ -15,18 +15,26 @@ module.exports = {
       type: "string",
       label: "Folder",
       description: "Choose a folder from the dropdown or turn structured mode off to enter a folder ID directly.",
-      async options(){
-        const folders = (await this.getFolders()).map(folder => {
-          return {
-            label: ' > ' + folder[0].FOLDER_NAME,
-            value: folder[0].FOLDER_ID,
-          }
-        })
-        // Zoho Docs API defaults to the root folder if no ID is submitted (i.e. value === null)
-        const root_folder = {label: 'Zoho Docs', value: null}
-        // Add the root folder as the first option before all the folders from getFolders()
-        const options = [root_folder, ...folders]
-        return options
+      async options({prevContext}){
+        const folders = await this.getSubfolders(prevContext.nextPageToken)
+        return {
+          options: folders.map(folder => folder.FOLDER_NAME || folder.FOLDERNAME), 
+          // options: [`${JSON.stringify(folders)}`],
+          context: {
+            nextPageToken: folders[0].FOLDER_ID || folders[0].FOLDERID,
+          },
+        }
+        // const folders = (await this.getFolders()).map(folder => {
+        //   return {
+        //     label: ' > ' + folder[0].FOLDER_NAME,
+        //     value: folder[0].FOLDER_ID,
+        //   }
+        // })
+        // // Zoho Docs API defaults to the root folder if no ID is submitted (i.e. value === null)
+        // const root_folder = {label: 'Zoho Docs', value: null}
+        // // Add the root folder as the first option before all the folders from getFolders()
+        // const options = [root_folder, ...folders]
+        // return options
       },
     },
     filePath: {
@@ -40,7 +48,7 @@ module.exports = {
     },
   },
   methods: {
-    async getFolders(folderid){
+    async getSubfolders(folderid){
       const {data} = await axios({
         method: "get",
         url: "https://apidocs.zoho.com/files/v1/folders",
@@ -51,8 +59,14 @@ module.exports = {
           folderid,
         },
       })
-      const [folderInfo, ...folders] = data
-      return folders
+      if(data.FOLDER){
+        return data.FOLDER
+      } else if (data.length){
+        const [folderInfo, ...folders] = data
+        return folders.map(folder => folder[0])
+      } else {
+        throw new Error(`Folder ID: ${folderid} Data: ${JSON.stringify(data)}`)
+      }
     },
     async uploadFile(folderID, filePath, fileName) {
       try {
