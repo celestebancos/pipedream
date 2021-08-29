9219ea93bb48ef393b6f26c8ee6b65d1388f84b4
Celeste Bancos <celestebancos@gmail.com>
2021-08-28 20:52:10 -0400

    Add common.js and comment out legacy code

--- components/zoho_docs/actions/find-folder/find-folder.js
+++ components/zoho_docs/actions/find-folder/find-folder.js
@@ -1,54 +1,57 @@
+const zohoDocs = require("../../zoho_docs.app.js");
+const common = require("../common.js");
+
 module.exports = {
 	name: "Find Folder",
 	description: "Search for a folder by name.",
 	key: "find-folder",
-	version: "0.0.1",
+	version: "0.0.3",
 	type: "action",
 	props: {
+    ...common.props,
 	},
 	methods: {
 	},
 	async run() {
-		if(params.existing_folder_id){
-			return params.existing_folder_id
-		} else {
-			// find the deal folder if it's not already linked in Airtable
-			// compile a list of candidate folders (all the folders in Load Processing and Loads in Storage)
-			const load_processing_folder_id = process.env.ZOHO_DOCS_LOAD_PROCESSING_FOLDER_ID
-			const load_processing = await getFolders(load_processing_folder_id)
-			const loads_in_storage = await getFolders(load_processing.find(folder => folder.name.includes('Loads in Storage')).id)
-			const all_folders = load_processing.concat(loads_in_storage)
+		// if(params.existing_folder_id){
+		// 	return params.existing_folder_id
+		// } else {
+		// 	// find the deal folder if it's not already linked in Airtable
+		// 	// compile a list of candidate folders (all the folders in Load Processing and Loads in Storage)
+		// 	const load_processing_folder_id = process.env.ZOHO_DOCS_LOAD_PROCESSING_FOLDER_ID
+		// 	const load_processing = await getFolders(load_processing_folder_id)
+		// 	const loads_in_storage = await getFolders(load_processing.find(folder => folder.name.includes('Loads in Storage')).id)
+		// 	const all_folders = load_processing.concat(loads_in_storage)
 
-			const po_number = params.po_number.replace('/', ' ')
-			const [matching_folder] = all_folders.filter(folder => folder.name.includes(po_number))
-			if(matching_folder){
-				console.log(matching_folder.name)
-				return matching_folder.id
-			} else {
-				throw new Error(`No folder found in Zoho Docs for PO ${po_number}`)
-			}
-		}
+		// 	const po_number = params.po_number.replace('/', ' ')
+		// 	const [matching_folder] = all_folders.filter(folder => folder.name.includes(po_number))
+		// 	if(matching_folder){
+		// 		console.log(matching_folder.name)
+		// 		return matching_folder.id
+		// 	} else {
+		// 		throw new Error(`No folder found in Zoho Docs for PO ${po_number}`)
+		// 	}
+		// }
 
-		async function getFolders(folder_id){
-			// console.log(`Getting list of folders in folder with id ${folder_id}`)
-			const response = await require("@pipedreamhq/platform").axios(this, {
-				method: "get",
-				url: `https://apidocs.zoho.com/files/v1/folders`,
-				headers: {
-					"Authorization": `Zoho-oauthtoken ${auths.zoho_docs.oauth_access_token}`,
-				},
-				params: {
-					folderid: folder_id,
-				},
-			})
-			const folders = response.FOLDER.map(folder => {
-				return {
-					name: folder.FOLDERNAME,
-					id: folder.FOLDERID,
-				}
-			})
-			return folders
-		}
+		// async function getFolders(folder_id){
+		// 	// console.log(`Getting list of folders in folder with id ${folder_id}`)
+		// 	const response = await require("@pipedreamhq/platform").axios(this, {
+		// 		method: "get",
+		// 		url: `https://apidocs.zoho.com/files/v1/folders`,
+		// 		headers: {
+		// 			"Authorization": `Zoho-oauthtoken ${auths.zoho_docs.oauth_access_token}`,
+		// 		},
+		// 		params: {
+		// 			folderid: folder_id,
+		// 		},
+		// 	})
+		// 	const folders = response.FOLDER.map(folder => {
+		// 		return {
+		// 			name: folder.FOLDERNAME,
+		// 			id: folder.FOLDERID,
+		// 		}
+		// 	})
+		// 	return folders
+		// }
 	},
 };
-
