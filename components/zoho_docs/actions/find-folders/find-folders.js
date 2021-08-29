c30c97356634e5a5d12b576b26678158937e84e8
Celeste Bancos <celestebancos@gmail.com>
2021-08-28 21:15:32 -0400

    Convert spaces to tabs

--- components/zoho_docs/actions/find-folders/find-folders.js
+++ components/zoho_docs/actions/find-folders/find-folders.js
@@ -8,29 +8,29 @@ module.exports = {
 	version: "0.1.5",
 	type: "action",
 	props: {
-    ...common.props,
-    searchTerm: {
-    	type: 'string',
-    	label: 'Search Term (Optional)',
-    	description: 'Any folder that contains the search term in its name will be returned. Leave blank to get all folders.',
-    	optional: true,
-    },
-    parentFolderId: {
-      propDefinition: [
-        zohoDocs,
-        "folderId",
-      ],
-      label: "Folder to Search In (Optional)",
-      description: "Leave this blank to search in the root folder. " +
-      "Choose a folder from the dropdown or turn structured mode off to enter a folder ID directly. " +
-      "Any folders in *Shared with Me* must be entered by ID as they will not be available on the dropdown.",
-      optional: true,
-    },
-    caseSensitive:{
-    	type: 'boolean',
-    	label: 'Case Sensitive',
-    	default: true
-    }
+		...common.props,
+		searchTerm: {
+			type: 'string',
+			label: 'Search Term (Optional)',
+			description: 'Any folder that contains the search term in its name will be returned. Leave blank to get all folders.',
+			optional: true,
+		},
+		parentFolderId: {
+			propDefinition: [
+				zohoDocs,
+				"folderId",
+			],
+			label: "Folder to Search In (Optional)",
+			description: "Leave this blank to search in the root folder. " +
+			"Choose a folder from the dropdown or turn structured mode off to enter a folder ID directly. " +
+			"Any folders in *Shared with Me* must be entered by ID as they will not be available on the dropdown.",
+			optional: true,
+		},
+		caseSensitive:{
+			type: 'boolean',
+			label: 'Case Sensitive',
+			default: true
+		}
 	},
 	methods: {
 	},
