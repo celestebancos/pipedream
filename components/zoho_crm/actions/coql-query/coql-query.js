6818addecfbb4626257af26fad52c427cbb45334
Celeste Bancos <celestebancos@gmail.com>
2021-11-20 13:02:44 -0500

    Convert SELECT prop to an array and add validation

--- components/zoho_crm/actions/coql-query/coql-query.js
+++ components/zoho_crm/actions/coql-query/coql-query.js
@@ -2,18 +2,18 @@ const zoho_crm = require("../../zoho_crm.app.js");
 
 module.exports = {
   name: "COQL Query",
-  version: "0.0.37",
+  version: "0.0.46",
   key: "zoho_crm-coql-query",
   description: "Get records via a COQL query. See [COQL Query API Docs](https://www.zoho.com/crm/developer/docs/api/v2/COQL-Overview.html) for more details on query construction.",
   props: {
     zoho_crm,
     select: {
       label: 'SELECT',
-      description: "Enter a [comma-separated list of field API names]" +
+      description: "Enter an array of [field API names]" +
         "(https://www.zoho.com/crm/developer/docs/api/v2/COQL-Overview.html) " +
         "to return as columns. Up to 50 columns are allowed. Unfortunately there is no shortcut to return all columns." +
-        "\nE.g. `Full_Name, Email, Phone, Account_Name.Account_Name`",
-      type: "string",
+        "\nE.g. `{{['Full_Name', 'Email', 'Phone', 'Account_Name.Account_Name']}}`",
+      type: "string[]",
     },
     from: {
       propDefinition: [
@@ -44,14 +44,20 @@ module.exports = {
   },
   type: "action",
   methods: {
+    removeDuplicates(array){
+      return array.filter((v, i, a) => a.indexOf(v) === i)
+    },
   },
   async run({ $ }) {
-    const select = 'SELECT ' + this.select
+    const select_fields = this.removeDuplicates(this.select)
+    if(select_fields.length > 50){
+      throw new Error(`Query Syntax Error: Too many fields in the SELECT field names list (must be 50 or fewer).`)
+    }
+    const select = 'SELECT ' + select_fields.join(', ')
     const from = 'FROM ' + this.from
     const where = 'WHERE ' + this.where
     const order_by = this.order_by ? 'ORDER BY ' + this.order_by : ''
     const query = [select, from, where, order_by].join('\n')
-    // const max_records = this.max_records
 
     const records = await this.zoho_crm.postCOQLQuery(query)
     $.export("query", query);
